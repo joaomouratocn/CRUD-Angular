90237b69881034668060e4cfcd1a4dfb56971b2d
joao-mourato <joaomouratocn@gmail.com>
2026-01-21 15:16:29 -0300

    created home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConsultComponent } from './consult-component/consult-component';
+import { RegisterComponent } from './register-component/register-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'consult', component:ConsultComponent},
+    {path:'register', component:RegisterComponent}
+];
